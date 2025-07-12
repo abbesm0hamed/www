4111e989eab87caf1daa043b03e71ad6de8b4a44
abbesm0hamed <abbesmohamed717@gmail.com>
2025-07-12 04:40:21 +0100

    feat: home page setup with sections

--- cmd/web/handlers/home.go
+++ cmd/web/handlers/home.go
@@ -3,9 +3,20 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/abbesm0hamed/portfolio/cmd/data"
+	"github.com/abbesm0hamed/portfolio/cmd/models"
 	"github.com/abbesm0hamed/portfolio/cmd/web/views"
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	views.Home().Render(r.Context(), w)
+	experiences := data.GetExperiences()
+	skills := data.GetSkills()
+	projects := data.GetProjects()
+	posts, err := data.LoadPosts()
+	if err != nil {
+		// Handle error - for simplicity, we'll log it and continue with an empty slice
+		posts = []*models.Post{}
+	}
+
+	views.Home(experiences, skills, projects, posts).Render(r.Context(), w)
 }
