12ebf2aad53133a52ba57749fce22e55c260a234
abbesm0hamed <abbesmohamed717@gmail.com>
2025-07-17 02:10:43 +0100

    feat: container query with tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./cmd/web/views/**/*.templ", "./cmd/web/components/**/*.templ"],
+  content: ["./cmd/web/**/*.{templ,go}", "./assets/**/*.{html,js}"],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "4rem",
+        md: "3rem",
+        lg: "2rem",
+        xl: "2rem",
+        "2xl": "1rem",
+      },
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
